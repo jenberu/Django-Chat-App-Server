4e520a53f323fa7a87de300ddb1fa4638eaab087
jenberu <jemberu0970@gmail.com>
2024-11-13 20:28:32 +0300

    consumer function is updated

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,14 +3,19 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 class ChatConsumer(WebsocketConsumer):
     def connect(self):#called when new connection is recived
+       # retrieves the room_id from the WebSocket connection’s UR
        self.id=self.scope['url_route']['kwargs']['room_id']
        self.room_group_name=f'chat_{self.id}'
+       #add a new websocket connection to a group
        async_to_sync(self.channel_layer.group_add)(self.room_group_name,self.channel_name)
        self.accept()
 
     def disconnect(self,close_code):
-           
-           pass
+       # leave room group
+       async_to_sync(self.channel_layer.group_discard)(
+                     self.room_group_name, self.channel_name
+                      )
+    
      # receive message from WebSocket
     def receive(self, text_data=None, bytes_data=None):
         # json.loads() to load the received JSON data into a Python dictionary
