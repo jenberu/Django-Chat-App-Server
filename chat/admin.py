964006ac67b28ed419af32bdb66b986db303f02c
jenberu <jemberu0970@gmail.com>
2024-11-13 23:21:08 +0300

    model updeted

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room,Message
+
 
 # Register your models here.
 admin.site.register(Room)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+ list_display = ['sent_on', 'user', 'room', 'content']
+ list_filter = ['sent_on', 'room']
+ search_fields = ['content']
+ raw_id_fields = ['user', 'content']
--- chat/models.py
+++ chat/models.py
@@ -3,11 +3,17 @@ from django.db import models
 from django.contrib.auth.models import User
 class Room(models.Model):
     name=models.CharField(max_length=100,unique=True)
+    def __str__(self):
+        return self.username
 
 class Message(models.Model):
-    room = models.ForeignKey(Room, related_name="messages", on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, related_name="chat_messages", on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT,related_name='chat_message')
     content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    sent_on = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+         return f'{self.user} on {self.room} at {self.sent_on}'
+
+
 
 
