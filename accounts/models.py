d7561e2bf5a32d2ceaff5411c6bbc52012f008f7
jenberu <jemberu0970@gmail.com>
2024-11-14 09:41:07 +0300

    custom mannager created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.core.exceptions import ObjectDoesNotExist
