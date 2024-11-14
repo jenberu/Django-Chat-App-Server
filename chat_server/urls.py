586ab606b19b3b3eb1662081f5e7f52aa7751a94
jenberu <jemberu0970@gmail.com>
2024-11-14 10:14:49 +0300

    updated

--- chat_server/urls.py
+++ chat_server/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls', namespace='chat')),
+    path('accounts/',include('accounts.urls',namespace='accounts'))
 
 ]
